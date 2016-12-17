af9e235adcfe562dc743bf124b5dbbe4cd3f4cc1
Ben197 <jabro888uc@Hotmail.com>
2016-12-17 21:14:44 +0100

    Initial Commit

---
+++ EmployeeData_1/src/main/java/nl/bp/database/MariaDB_SQL.java
@@ -0,0 +1,12 @@
+package nl.bp.database;
+
+public class MariaDB_SQL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testje");
+	}
+	
+	
+
+}
